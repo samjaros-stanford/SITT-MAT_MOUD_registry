d15bc98866bd7b851e495b7788fb218f252d3c67
samjaros-stanford <76895253+samjaros-stanford@users.noreply.github.com>
2023-06-14 13:45:11 -0700

    Update to quarterly IMAT calculations

--- code/04_calc_IMAT.R
+++ code/04_calc_IMAT.R
@@ -1,7 +1,41 @@
+library(lubridate)
 library(tidyverse)
 
-# Until additional IMAT data are collected, the baseline IMAT data are current
-IMAT = readRDS("data/current_baseline_reaim-imat.rds") %>%
-  filter(startsWith(variable, "imat"))
+##########
+# Import #
+##########
+raw_imat = read.csv(here::here("raw_data/IMAT-all_20230613_SUD.csv")) %>%
+  bind_rows(read.csv(here::here("raw_data/IMAT-all_20230613_PC.csv"))) %>%
+  # Get survey date from redcap event
+  mutate(date = my(substr(redcap_event_name, 1, 8))) %>%
+  # Select only desired columns (no comment columns)
+  select(program_id, date, starts_with("imat_d"), imat_total_mean, -ends_with("_c")) %>%
+  # Remove rows where all data are NA
+  filter(if_any(starts_with("imat"), ~!is.na(.x)))
 
-saveRDS(IMAT, "data/current_imat.rds")
+#############
+# All items #
+#############
+
+item_imat = raw_imat %>%
+  select(-ends_with("_mean")) %>%
+  pivot_longer(-c(program_id, date),
+               names_to="variable",
+               values_to="value")
+
+saveRDS(item_imat, "data/current_imat_item.rds")
+
+#############
+# Subscales #
+#############
+
+# Flip to longer form
+subscale_imat = raw_imat %>%
+  select(date, program_id, ends_with("_mean")) %>%
+  pivot_longer(ends_with("_mean"),
+               names_pattern="(.*)_mean",
+               names_to="variable",
+               values_to="value")
+
+saveRDS(subscale_imat, "data/current_imat_subscale.rds")
+
