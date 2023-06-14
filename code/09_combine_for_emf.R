5f5a112930f26d0ae5aa0eb6dce1823f3cebb6b3
samjaros-stanford <76895253+samjaros-stanford@users.noreply.github.com>
2023-06-14 13:51:56 -0700

    Move EMF calculations to end; do clinic type assignment here

---
+++ code/09_combine_for_emf.R
@@ -0,0 +1,23 @@
+# Data need to be in long format for use in EMF
+# This file can be changed to produce different data sets that update
+#   alongside the current CDI, REAIM, and IMAT datasets
+
+library(stringr)
+library(tidyverse)
+
+# Prep REAIM
+#   Add clinic type
+reaim = readRDS("data/current_reaim.rds") %>%
+  mutate(type = if_else(program_id %in% paste0("id", str_pad(0:49, 2, "left", "0")),
+                                                                "SUD",
+                                                                "PC"))
+
+# Prep IMAT
+#   Add clinic type
+imat = readRDS("data/current_imat_subscale.rds") %>%
+  mutate(type = if_else(program_id %in% paste0("id", str_pad(0:49, 2, "left", "0")),
+                        "SUD",
+                        "PC"))
+
+# Create data set for "Quarterly REAIM/IMAT Reports" (QRIR)
+write.csv(rbind(reaim, imat), "data/current_QRIR.csv", row.names=F)
