e3ed89fc92c63586b360494ad4cb2c1856294e1a
samjaros-stanford <76895253+samjaros-stanford@users.noreply.github.com>
2023-06-15 13:31:44 -0700

    Create runner file that will load packages and run all files in order

---
+++ code/_runner.R
@@ -0,0 +1,25 @@
+##########
+# Install all required packages
+# Runs all scripts required for complete SITT-MAT analysis
+##########
+
+# Clear environemnt
+rm(list=ls())
+
+# TODO: implement renv?
+
+####################
+# Install packages #
+####################
+packages = c("htmlTable", "lubridate", "readxl", "stringr", "tidyverse")
+install.packages(packages)
+
+##########################################
+# Run all code that starts with a number #
+##########################################
+### NOTE: NOT FULLY WORKING, USE AT YOUR OWN RISK ###
+files = list.files("code", pattern="^\\d.+\\.R")
+
+for(file in files){
+  source(paste0(getwd(),"/code/",file))
+}
