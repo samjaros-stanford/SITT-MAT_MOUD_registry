377361f1947bdad23ca87041bd1a02014882324d
samjaros-stanford <76895253+samjaros-stanford@users.noreply.github.com>
2023-07-12 13:48:35 -0700

    Align reaim C1 calculations

--- code/a_combine_manual_automatic_reaim.R
+++ code/a_combine_manual_automatic_reaim.R
@@ -22,3 +22,33 @@ full_data = rbind(manual_data, trimmed_baseline) %>%
 
 # Save data to file
 write.csv(full_data, "data/reaim-imat_baseline_manual.csv", row.names=F)
+
+
+##############
+# Correct reaim_c1
+#
+# Sep-Nov c1 was calculated # <72h to MOUD/b2
+# Dec-Feb c1 was calculated # <72h to MOUD/b4
+# Need to correct Sep-Nov to match Dec-Feb
+##############
+library(lubridate)
+library(tidyverse)
+
+bad_manual_file = read.csv("raw_data/MANUAL_SITT-MAT_Quarterly Data Reports - Completed Manuals.csv") %>%
+  select(-c(Maryam_cal, Hannah_cal, notes)) %>%
+  pivot_wider(id_cols=c(program_id, date, type),
+              names_from=variable,
+              values_from=final.value,
+              values_fn = as.numeric)
+
+good_manual_file = bad_manual_file %>%
+  filter(date %in% c("Sep-22", "Oct-22", "Nov-22")) %>%
+  mutate(reaim_c1 = (reaim_c1/100)*reaim_b2/reaim_b4) 
+  rbind(bad_manual_file %>% filter(date %in% c("Dec-22", "Jan-23", "Feb-23"))) %>%
+  pivot_longer(cols=-c(date, program_id, type),
+               names_to="variable",
+               values_to="final_value") %>%
+  arrange(program_id, variable, my(date)) %>%
+  select(date, program_id, variable, final_value, type)
+
+write.csv(good_manual_file, "data/corrected_manual_calculations_Apr23.csv", row.names=F)
