843fca1bdfa32d041a06dc48caf47d2a96264162
samjaros-stanford <samjaros@stanford.edu>
2023-08-28 21:17:06 -0700

    Revert "Add renv to project for version control"
    This reverts commit 0ecaadff833f111a2a35625d656ddf6bf17d40a3.
    
    Reverting addition of renv for version control as it was improperly
    committed

--- renv/library/R-4.3/x86_64-w64-mingw32/bigD/NEWS.md
+++
@@ -1,3 +0,0 @@
-# bigD 0.2.0
-
-* New package with everything you need to format dates and times.
--- renv/library/R-4.3/x86_64-w64-mingw32/rJava/jri/run.bat
+++
@@ -1,2 +0,0 @@
-set PATH=%PATH%;D:/RCompile/recent/R-4.3.1\bin;D:/RCompile/recent/R-4.3.1\lib
-C:/PROGRA~1/ADOPTO~1/JDK-11~1.9-H/bin/java -cp .;examples;JRI.jar;src/JRI.jar rtest $*
